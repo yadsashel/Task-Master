87093884e528f526d9bfcb0e56a34e420d3a6d1d
yadsashel <ysalhi600@gmail.com>
2024-10-31 11:48:20 +0100

    fixing bugs

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,7 @@
-import os
 from pymongo import MongoClient
 
 def get_db_connection():
-    mongo_uri = os.getenv("MONGO_URI")
-    client = MongoClient(mongo_uri)
-    db = client['taskmaster_db']
+    # Be sure to URL-encode any special characters in the username/password
+    client = MongoClient("mongodb+srv://taskmaster_db:2002%40Yad@cluster0.d1s6d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") 
+    db = client['taskmaster_db']  # Use square brackets to access the database
     return db
