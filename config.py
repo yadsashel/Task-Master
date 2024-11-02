62f41f5a35e1072d5be7ccdcaa726e87082a5994
yadsashel <ysalhi600@gmail.com>
2024-11-02 14:59:06 +0100

    fixing some error connections

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 def get_db_connection():
-    uri = "mongodb+srv://taskmaster_db:2002%40Yad@cluster0.d1s6d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    # Use the environment variable for MongoDB URI
+    uri = os.getenv("MONGODB_URI")
+    if not uri:
+        raise ValueError("MONGODB_URI not set in .env file")
+    
     client = MongoClient(uri, tlsAllowInvalidCertificates=True)  # Allow insecure SSL
     db = client['taskmaster_db']  # Specify your database name
     return db
